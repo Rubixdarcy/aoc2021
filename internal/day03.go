afd16acfb7848e5d256b033f304b6e460b51cb5f
Darcy Ryan <darcy150@gmail.com>
2021-12-07 16:06:43 +1100

    Day 3 Part 2

--- internal/day03.go
+++ internal/day03.go
@@ -4,6 +4,8 @@ import (
     "fmt"
     "bufio"
     "os"
+    "sort"
+    "strconv"
 )
 
 const nbits = 12
@@ -42,4 +44,59 @@ func Day3Part1() {
 }
 
 func Day3Part2() {
+    f, err := os.Open("input/day03.txt")
+    check(err)
+    defer f.Close()
+
+    scanner := bufio.NewScanner(f)
+    var nums []string
+    for scanner.Scan() {
+        nums = append(nums, scanner.Text())
+    }
+    sort.Strings(nums)
+
+    // Oxygen
+    var oxygen int64
+    oxygenSlice := nums
+    for i := 0; i < nbits; i++ {
+        first1 := indexOfFirst1InCol(oxygenSlice, i)
+        if first1 * 2 <= len(oxygenSlice) {
+            oxygenSlice = oxygenSlice[first1:]
+        } else {
+            oxygenSlice = oxygenSlice[:first1]
+        }
+        if len(oxygenSlice) == 1{
+            oxygen, err = strconv.ParseInt(oxygenSlice[0], 2, 64)
+            check(err)
+            break
+        }
+    }
+
+    // CO2
+    var co2 int64
+    co2Slice := nums
+    for i := 0; i < nbits; i++ {
+        first1 := indexOfFirst1InCol(co2Slice, i)
+        if first1 * 2 <= len(co2Slice) {
+            co2Slice = co2Slice[:first1]
+        } else {
+            co2Slice = co2Slice[first1:]
+        }
+        if len(co2Slice) == 1{
+            co2, err = strconv.ParseInt(co2Slice[0], 2, 64)
+            check(err)
+            break
+        }
+    }
+
+    fmt.Println(oxygen * co2)
+}
+
+func indexOfFirst1InCol(nums []string, col int) int {
+    for i, s := range nums {
+        if s[col] == '1' {
+            return i
+        }
+    }
+    return len(nums)
 }
