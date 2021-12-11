87c8225ed70afb76400511fa5aee99a67b53bc51
Darcy Ryan <darcy150@gmail.com>
2021-12-11 12:24:18 +1100

    Day 10 Part 2

--- internal/day10.go
+++ internal/day10.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "bufio"
     "os"
+    "sort"
 )
 
 const (
@@ -39,6 +40,13 @@ var delimClassScores = map[delimClass]int{
     runeDelimClasses['>']: 25137,
 }
 
+var shapeCompleteScores = map[int]int {
+    delimShapeParen: 1,
+    delimShapeSquare: 2,
+    delimShapeBrace: 3,
+    delimShapeAngle: 4,
+}
+
 func Day10Part1() {
     f, err := os.Open("input/day10.txt")
     check(err)
@@ -68,4 +76,37 @@ func Day10Part1() {
 }
 
 func Day10Part2() {
+    f, err := os.Open("input/day10.txt")
+    check(err)
+    defer f.Close()
+    scanner := bufio.NewScanner(f)
+
+    var scores []int
+    for scanner.Scan() {
+        var shapes []int
+        invalid := false
+        for _, r := range scanner.Text() {
+            class := runeDelimClasses[r]
+
+            if class.dir == delimDirOpen {
+                shapes = append(shapes, class.shape)
+            } else {
+                if shapes[len(shapes) - 1] != class.shape {
+                    invalid = true
+                    break
+                }
+                shapes = shapes[:len(shapes) - 1]
+            }
+        }
+        if !invalid {
+            score := 0
+            for i, _ := range shapes {
+                score = score * 5 + shapeCompleteScores[shapes[len(shapes) - i - 1]]
+            }
+            scores = append(scores, score)
+        }
+    }
+
+    sort.Ints(scores)
+    fmt.Println(scores[len(scores) / 2])
 }
